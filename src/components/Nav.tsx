54f83ee4d4ecaaa7585d53dc9e8450b9d3b59b45
johnwgomez <johnwgomez10@gmail.com>
2025-05-31 17:08:03 -0700

    everything set up correctly now need to test

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom';
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
+    // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
-  )
+    <nav className="nav-container">
+      <ul className="nav-list">
+        <li>
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/potentialcandidates">
+            Potential Candidates
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,12 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export type Candidate = {
+    id: number;
+    html_url: string | undefined;
+    login: string;
+    avatar_url: string;
+    name?: string;
+    location?: string;
+    email?: string;
+    company?: string;
+    bio?: string;
+  };
